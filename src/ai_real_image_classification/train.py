ef369fa6a82cd522476721491507d8035e80bb9e
RapoVan <79799482+RapoVan@users.noreply.github.com>
2026-01-09 14:56:31 +0100

    Added cookiecutter template
    Added cookiecutter template using mlops template provided by the course

---
+++ src/ai_real_image_classification/train.py
@@ -0,0 +1,10 @@
+from ai_real_image_classification.model import Model
+from ai_real_image_classification.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
