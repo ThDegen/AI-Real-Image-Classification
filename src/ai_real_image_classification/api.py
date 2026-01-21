c85c8f4d2ecfc2deac4b356c75be2a0fdac2b520
Yeren <yerenkl@gmail.com>
2026-01-21 10:48:41 +0100

    api ruff fix

--- src/ai_real_image_classification/api.py
+++ src/ai_real_image_classification/api.py
@@ -3,9 +3,11 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "AI vs Human Image Classification API"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
